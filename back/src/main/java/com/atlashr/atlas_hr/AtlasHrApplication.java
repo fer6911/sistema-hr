6bf6ae064663eb4b406ffb995915440a4394cef9
fer6911 <pablo6911@outlook.com>
2026-07-17 10:47:04 -0500

    (back) first commit

---
+++ back/src/main/java/com/atlashr/atlas_hr/AtlasHrApplication.java
@@ -0,0 +1,13 @@
+package com.atlashr.atlas_hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasHrApplication.class, args);
+	}
+
+}
