4c1eb87fd037f1b1d906a00f1acd5b4b25c8f14f
fer6911 <pablo6911@outlook.com>
2026-07-17 19:03:28 -0500

    feat: agrega findById a puerto empleado

--- back/src/main/java/com/atlashr/atlas_hr/application/ports/out/EmpleadoRepositoryPort.java
+++ back/src/main/java/com/atlashr/atlas_hr/application/ports/out/EmpleadoRepositoryPort.java
@@ -3,10 +3,12 @@ package com.atlashr.atlas_hr.application.ports.out;
 import com.atlashr.atlas_hr.domain.model.Empleado;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmpleadoRepositoryPort {
     boolean existsByEmail(String email);
     boolean existsById(Long id);
+    Optional<Empleado> findById(Long id);
     Empleado save(Empleado empleado);
     List<Empleado> findAll();
 }
